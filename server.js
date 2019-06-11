5f6159cb97b4f106d28d36e2cc3866b961ffa4fe
Frankzhac <Frank.hemle@gmail.com>
2019-06-11 09:34:07 -0400

    server.get pulls accounts

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const db = require('./data/accounts-model')
 
 const server = express();
+server.use(express.json());
 
 // your code here
+const port = 4000;
+
+server.get('/', (req, res) => {
+  res.send('Hello Word');
+});
+
+// Accounts Model start below
+
+// Get
+server.get('/accounts', (req, res) => {
+  db.find()
+    .then(accounts => {
+      res.json({ accounts });
+    })
+    .catch(err => {
+      // console.log(err);
+      res.status(500).json({
+        error: "The users information could not be retrieved."
+      });
+    });
+});
 
 module.exports = server;
