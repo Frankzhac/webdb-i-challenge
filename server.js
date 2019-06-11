67e3fc6029a1d0bb09bca1dcba151130ab3fd8c5
Frankzhac <Frank.hemle@gmail.com>
2019-06-11 10:14:46 -0400

    fixed my budget db to autoincrement and created post action

--- server.js
+++ server.js
@@ -27,4 +27,48 @@ server.get('/accounts', (req, res) => {
     });
 });
 
+server.get('/accounts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(account => {
+      if (account.length === 0) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist."
+        });
+      } else {
+        res.json(account);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+         error: "The user information could not be retrieved."
+      });
+    });
+});
+
+server.post('/accounts', async (req, res) => {
+  const { name, budget } = req.body;
+  if (!name || !budget) {
+    res.status(400).json({
+      errorMessage: "Please provide name and budget for the user."
+    });
+  }
+  // add/save new user in the db
+
+  db.add(
+    req.body
+  )
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(err => {
+        // console.log(err);
+        res.status(500).json({
+          success: false,
+          error: "There was an error while saving the user to the database",
+        });
+      });
+});
+
 module.exports = server;
